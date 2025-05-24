2625e7439b1bb48f3e606b5ecc257c97c609f4f5
Holl4444 <holly.cello@gmail.com>
2025-05-24 21:44:45 +0100

    fix: missing strictMode

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,13 @@
-// import { StrictMode } from 'react';
+import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { GameProvider } from './utils/context/GameProvider.tsx';
 import './index.css';
 import App from './App.tsx';
 
 createRoot(document.getElementById('root')!).render(
- 
+  <StrictMode>
     <GameProvider>
       <App />
     </GameProvider>
-
+  </StrictMode>
 );
-{/* <StrictMode></StrictMode>; */}
